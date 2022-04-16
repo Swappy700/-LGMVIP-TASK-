c0ada17cda31b0fc24e5256bfc2b418c0baa9202
Swappy700 <102027785+Swappy700@users.noreply.github.com>
2022-04-16 18:52:04 +0530

    Add files via upload

---
+++ TASK 2/src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return(
+        
+        <div style={{height: 'auto'}}>
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll;
+
+
